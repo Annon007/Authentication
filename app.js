f9512a3d328a4b70037398e82ba06f4167346004
Annon007 <62867777+Annon007@users.noreply.github.com>
2021-05-07 05:20:37 +0600

    Add Environment files

---
+++ app.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const encrypt=require("mongoose-encryption")
+const app = express();
+console.log(process.env.SECRET);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true }
+});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET,encryptedFields: ["password"]});
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function (req, res) {
+    res.render("home");
+})
+
+
+app.route("/register")
+    .get(function (req, res) {
+        res.render("register");
+    })
+    .post(function (req, res) {
+        const newUser = new User({
+            email: req.body.username,
+            password: req.body.password
+        });
+        newUser.save(function (err) {
+            if (!err) {
+                res.render("login");
+            } else {
+                console.log(err);
+            }
+        });
+    });
+app.route("/login")
+    .get(function (req, res) {
+        res.render("login");
+    })
+    .post(function(req,res){
+        const username= req.body.username;
+        const password= req.body.password;
+        User.findOne({email:username},function(err,found){
+            if(!err){
+                if(found){
+                    if(found.password===password){
+                        res.render("secrets")
+                    }
+                }
+            }else{
+                cpnsole.log(err);
+            }
+        });
+
+    });
+app.listen("3000", function () {
+    console.log("Server is running on Port 3000");
+})
